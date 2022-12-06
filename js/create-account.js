b863e07cf11381d540c1b589e15945ec1700cbf4
Ilona R <ilona.raugalaite@yahoo.co.uk>
2022-12-06 13:09:20 +0100

    add success msg - create account success

--- js/create-account.js
+++ js/create-account.js
@@ -14,6 +14,8 @@ const createAccountFormUserEmail = document.getElementById(
 const createAccountFormUserPassword = document.getElementById(
   'createAccountFormUserPassword'
 );
+const createAccountSuccessMsg = document.getElementById('success-message');
+const createAccountFormContainerDiv = document.getElementById('createAccountFormContainerDiv');
 
 createAccountForm.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -37,7 +39,7 @@ createAccountForm.addEventListener('submit', function (e) {
                                     </svg>
                                   </div>
                                   <div class="ml-3">
-                                    <h3 class="text-sm font-medium text-red-800">Required email with noroff.no/stud.noroff.no</h3>
+                                    <h3 class="text-sm font-medium text-red-800">Missing user name</h3>
                                   </div>
                                 </div>
                               </div>
@@ -74,7 +76,6 @@ createAccountForm.addEventListener('submit', function (e) {
     createAccountFormError.innerHTML += `<div class="rounded-md bg-red-50 p-4">
                                 <div class="flex">
                                   <div class="flex-shrink-0">
-                                    <!-- Heroicon name: mini/x-circle -->
                                     <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
                                     </svg>
@@ -96,7 +97,7 @@ createAccountForm.addEventListener('submit', function (e) {
       password: createAccountFormUserPassword.value,
     };
 
-    console.log(createUserInfo);
+    // console.log(createUserInfo);
 
     (async function registerUser() {
       const response = await fetch(REGISTER_USER_API, {
@@ -107,12 +108,34 @@ createAccountForm.addEventListener('submit', function (e) {
         },
       });
 
-      const data = await response.json();
-      console.log(data);
+      // const data = await response.json();
+      // console.log(data);
 
       if (response.status === 201) {
         console.log('user created');
-        location.replace('/index.html');
+        // location.replace('/index.html');
+        createAccountForm.classList.add('hidden');
+        navMenuDropdown.classList.add('hidden');
+        navMenuMobileSvgExit.classList.add('hidden');
+        navMenuMobileSvg.classList.remove('hidden');
+        createAccountFormContainerDiv.classList.add('hidden');
+
+        createAccountSuccessMsg.classList.remove('hidden');
+        createAccountSuccessMsg.innerHTML = `<div class="rounded-md bg-green-50 p-4">
+                                              <div class="flex">
+                                                <div class="flex-shrink-0">
+                                                  <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                                                  </svg>
+                                                </div>
+                                                <div class="ml-3">
+                                                  <h3 class="text-sm font-medium text-green-800">Congratulations!!</h3>
+                                                  <div class="mt-2 text-sm text-green-700">
+                                                    <p>You have successfully created your new account. Go to log in to start sharing products and start bidding</p>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>`;
       } else if (response.status === 400) {
         createAccountFormError.classList.remove('hidden');
         createAccountFormError.innerHTML = `<div class="border-l-4 border-yellow-400 bg-yellow-50 p-4">
