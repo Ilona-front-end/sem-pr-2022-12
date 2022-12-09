0669cb98fa3d827a94fa3dda26eea1701f87f8b6
Ilona R <ilona.raugalaite@yahoo.co.uk>
2022-12-09 10:03:07 +0100

    my products html - delete add, success msg add

--- js/all-user-products.js
+++ js/all-user-products.js
@@ -1,4 +1,4 @@
-import { ALL_USER_PRODUCTS_BASE } from './settings/api';
+import { ALL_USER_PRODUCTS_BASE, DELETE_PRODUCT_BASE } from './settings/api';
 import { getUserName, getToken } from './utils/storage';
 
 const allUserProductsContainer = document.getElementById('all-products-container');
@@ -6,7 +6,7 @@ const allUserProductsContainer = document.getElementById('all-products-container
 const userName = getUserName();
 const accessToken = getToken();
 
-const ALL_USER_PRODUCTS_API = `${ALL_USER_PRODUCTS_BASE}${userName}?_listings=true`;
+const ALL_USER_PRODUCTS_API = `${ALL_USER_PRODUCTS_BASE}/${userName}?_listings=true`;
 
 async function getUserActivity() {
   const singleUserProductsResponse = await fetch(ALL_USER_PRODUCTS_API, {
@@ -26,6 +26,23 @@ async function getUserActivity() {
 
     const numberOfUserProducts = userProductsListing.length;
     console.log('numberOfUserProducts', numberOfUserProducts);
+
+    if (numberOfUserProducts === 0) {
+      allUserProductsContainer.innerHTML = `<div class="border-l-4 border-yellow-400 bg-yellow-50 p-4">
+                                              <div class="flex">
+                                                <div class="flex-shrink-0">
+                                                  <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd" d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                                                  </svg>
+                                                </div>
+                                                <div class="ml-3">
+                                                  <p class="text-sm text-yellow-700">
+                                                    No products published yet.
+                                                  </p>
+                                                </div>
+                                              </div>
+                                            </div>`;
+    }
 
     for (let i = 0; i < numberOfUserProducts; i++) {
       // const created = userProductsListing[i];
@@ -47,4 +64,51 @@ async function getUserActivity() {
     allUserProductsContainer.innerHTML += 'Error';
   }
 }
-getUserActivity();
+
+getUserActivity().then(() => {
+  const deleteBtn = document.querySelectorAll('.delete-btn');
+  deleteBtn.forEach((btn) => {
+    btn.addEventListener('click', function () {
+      const productId = this.dataset.id;
+      console.log(productId)
+      deleteProductFunction(productId);
+    });
+  });
+});
+
+function deleteProductFunction(id) {
+  const deleteProduct = async () => {
+    let response = await fetch(`${DELETE_PRODUCT_BASE}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    console.log(response.status);
+    console.log('Product deleted');
+
+    const deleteSuccessMsg = document.getElementById('delete-success-message');
+
+    deleteSuccessMsg.innerHTML = `<div class="rounded-md bg-green-50 p-4">
+                                            <div class="flex">
+                                              <div class="flex-shrink-0">
+                                                <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                                                </svg>
+                                              </div>
+                                              <div class="ml-3">
+                                                <h3 class="text-sm font-medium text-green-800">Congratulations!!</h3>
+                                                <div class="mt-2 text-sm text-green-700">
+                                                  <p>Product was deleted</p>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>`;
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+  };
+  deleteProduct().then((x) => {
+  });
+}
--- js/settings/api.js
+++ js/settings/api.js
@@ -1,10 +1,12 @@
-const API_BASE = 'https://api.noroff.dev/api/v1/auction/';
+const API_BASE = 'https://api.noroff.dev/api/v1/auction';
 
-const GET_PRODUCTS_URL = `${API_BASE}listings`;
-const LOGIN_USER_API = `${API_BASE}auth/login`;
-const REGISTER_USER_API = `${API_BASE}auth/register`;
-const ADD_PRODUCT_API = `${API_BASE}listings`;
-const ALL_USER_PRODUCTS_BASE = `${API_BASE}profiles/`;
-// const ALL_USER_PRODUCTS_API = `${API_BASE}profiles/${userName}?_listings=true`;
+const GET_PRODUCTS_URL = `${API_BASE}/listings`;
+const LOGIN_USER_API = `${API_BASE}/auth/login`;
+const REGISTER_USER_API = `${API_BASE}/auth/register`;
+const ADD_PRODUCT_API = `${API_BASE}/listings`;
+const ALL_USER_PRODUCTS_BASE = `${API_BASE}/profiles`;
+// const ALL_USER_PRODUCTS_API = `${API_BASE}/profiles/${userName}?_listings=true`;
+const DELETE_PRODUCT_BASE = `${API_BASE}/listings`;
+// ${API_BASE}/listings/${id}
 
-export { API_BASE, GET_PRODUCTS_URL, LOGIN_USER_API, REGISTER_USER_API, ADD_PRODUCT_API, ALL_USER_PRODUCTS_BASE };
+export { API_BASE, GET_PRODUCTS_URL, LOGIN_USER_API, REGISTER_USER_API, ADD_PRODUCT_API, ALL_USER_PRODUCTS_BASE, DELETE_PRODUCT_BASE };
