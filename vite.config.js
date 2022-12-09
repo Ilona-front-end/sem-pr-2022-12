b4e300e012ae06c66da8d3381f4decdc4abf26ca
Ilona R <ilona.raugalaite@yahoo.co.uk>
2022-12-09 17:52:00 +0100

    profile page sceleton

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default {
         singleProduct: resolve(__dirname, 'single-product.html'),
         addProduct: resolve(__dirname, 'add-product.html'),
         allUserProducts: resolve(__dirname, 'all-user-products.html'),
+        profile: resolve(__dirname, 'profile.html'),
       },
     },
   },
