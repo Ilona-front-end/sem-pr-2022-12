1e007f32f3cd434780d07003a362a0253ee12372
Ilona R <ilona.raugalaite@yahoo.co.uk>
2022-12-16 04:42:44 +0100

    email validation change, error message add (add-product.js)

--- js/add-product.js
+++ js/add-product.js
@@ -18,6 +18,7 @@ createNewProductForm.addEventListener('submit', function (event) {
 
   let isProductName = false;
   let isBiddingEnd = false;
+  let isProductDescriptionLength = false;
   let approveCreateProduct = false;
 
   if (productName.value.trim().length > 3) {
@@ -56,7 +57,25 @@ createNewProductForm.addEventListener('submit', function (event) {
                                 </div>`;
   }
 
-  approveCreateProduct = isProductName && isBiddingEnd;
+  if (productDescription.value.length < 280) {
+    isProductDescriptionLength = true;
+  } else {
+    isProductDescriptionLength = false;
+    generalMessage.innerHTML += `<div class="rounded-md bg-red-50 p-4">
+                                  <div class="flex">
+                                    <div class="flex-shrink-0">
+                                      <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
+                                      </svg>
+                                    </div>
+                                    <div class="ml-3">
+                                      <h3 class="text-sm font-medium text-red-800">Description cannot be greater than 280 characters</h3>
+                                    </div>
+                                  </div>
+                                </div>`;
+  }
+
+  approveCreateProduct = isProductName && isBiddingEnd && isProductDescriptionLength;
 
   let productData = {};
 
